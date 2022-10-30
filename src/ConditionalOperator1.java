019c4130b52e7cf1b3c33ae6310b89904840b8a4
harlamenkoel <harlamenkoel@gmail.com>
2022-10-30 19:48:53 +0700

    HomeWork 1 Task 2

--- src/ConditionalOperator1.java
+++ src/ConditionalOperator1.java
@@ -12,5 +12,20 @@ public class ConditionalOperator1 {
         } else {
             System.out.println("Вы еще малы, нужно подождать");
         }
+
+        System.out.println();
+        System.out.println("Домашняя работа 1.Задание 2");
+        System.out.print("Введите ваш возраст: ");
+        int age2 = scanner.nextInt();
+
+        if (age2 >= 7 && age2 < 18) {
+            System.out.println("Вы ребенок, и ходите в школу");
+        } else if (age2 >= 18 && age2 <= 23) {
+            System.out.println("Вы закончили школу и можете отправляться в университет");
+        } else if (age2 >= 24) {
+            System.out.println("Вы окончили университет и вам пора искать первую работу");
+        }else {
+            System.out.println("Вы слишком малы");
+        }
     }
 }
