d230c40fd9385b7de248236c9e913f9d83154738
harlamenkoel <harlamenkoel@gmail.com>
2022-10-30 19:32:36 +0700

    HomeWork 1 Task 1

--- src/ConditionalOperator1.java
+++ src/ConditionalOperator1.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class ConditionalOperator1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Домашняя работа 1.Задание 1");
+        System.out.print("Введите ваш возраст: ");
+        int age = scanner.nextInt();
 
-        System.out.println("Hello world!");
+        if (age >= 18) {
+            System.out.println("Поздравляем вы совершенноле́тний");
+        } else {
+            System.out.println("Вы еще малы, нужно подождать");
+        }
     }
 }
