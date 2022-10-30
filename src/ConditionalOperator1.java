3a1a3adb3ba49b2ae4fe3e7689abc8b997baa26e
harlamenkoel <harlamenkoel@gmail.com>
2022-10-30 22:09:07 +0700

    Задачи повторяються, я сразу выполнил с операторами else if(лучше всегда делать так т. к. есть гарантия что хоть раз в какой- либо из вариантов зайдет)

--- src/ConditionalOperator1.java
+++ src/ConditionalOperator1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ConditionalOperator1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Домашняя работа 1.Задание 1");
+        System.out.println("Задание 1");
         System.out.print("Введите ваш возраст: ");
         int age = scanner.nextInt();
 
@@ -14,22 +14,24 @@ public class ConditionalOperator1 {
         }
 
         System.out.println();
-        System.out.println("Домашняя работа 1.Задание 2");
+        System.out.println("Задание 2");
         System.out.print("Введите ваш возраст: ");
         int age2 = scanner.nextInt();
 
-        if (age2 >= 7 && age2 < 18) {
-            System.out.println("Вы ребенок, и ходите в школу");
+        if (age2 >= 2 && age2 <= 6) {
+            System.out.println("Если возраст человека равен " + age2 + " , то ему нужно ходить в детский сад");
+        } else if (age2 >= 7 && age2 < 18) {
+            System.out.println("Если возраст человека равен " + age2 + " , то ему нужно ходить в школу");
         } else if (age2 >= 18 && age2 <= 23) {
-            System.out.println("Вы закончили школу и можете отправляться в университет");
+            System.out.println("Если возраст человека равен " + age2 + " , то ему нужно ходить в университет");
         } else if (age2 >= 24) {
-            System.out.println("Вы окончили университет и вам пора искать первую работу");
+            System.out.println("Если возраст человека равен " + age2 + " , то ему нужно ходить на работу");
         } else {
             System.out.println("Вы слишком малы");
         }
 
         System.out.println();
-        System.out.println("Домашняя работа 1.Задание 3");
+        System.out.println("Задание 3");
         System.out.print("Введите количество пассажиров в вагоне: ");
         int amountPassengers = scanner.nextInt();
         if (amountPassengers <= 59) {
@@ -38,6 +40,41 @@ public class ConditionalOperator1 {
             System.out.println("В вагоне остались только стоячие места");
         } else {
             System.out.println("В вагоне мест больше нет");
+        }
+
+        System.out.println();
+        System.out.println("Задание 4");
+        System.out.print("Введите возраст, чтобы определить условия катания на атракционе: ");
+        int age3 = scanner.nextInt();
+
+        if (age3 <= 5) {
+            System.out.println("Вы слишком малы и не можете кататься на атракционе");
+        } else if (age3 <= 14) {
+            System.out.println("Вы может кататься только в сопровождении взрослого. " +
+                    "Если взрослого нет, то кататься нельзя");
+        } else {
+            System.out.println("Вы старше 14 лет, можете кататься без сопровождения взрослого");
+        }
+
+        System.out.println();
+        System.out.println("Задание 5");
+        System.out.println("Введите 3 числа программа определит наибольшее из них");
+
+        System.out.print("Введите первое число: ");
+        int one = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int two = scanner.nextInt();
+
+        System.out.print("Введите третье число: ");
+        int free = scanner.nextInt();
+
+        if (one > two) {
+            System.out.println("Число " + one + " больше");
+        } else if (two > one && two > free) {
+            System.out.println("Число " + two + " больше");
+        } else {
+            System.out.println("Число " + free + " больше");
         }
     }
 }
